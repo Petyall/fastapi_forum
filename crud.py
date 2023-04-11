1d0f459aca6dad52c7a2fa5c6f3ce46f018279c0
petyal <alekolen35@gmail.com>
2023-04-11 18:45:36 +0300

    JWT auth done & async added

--- crud.py
+++ crud.py
@@ -19,7 +19,6 @@ def get_db():
     finally:
         db.close()
 
-
 def create_database():
     """Функция, создающая БД, если она не существует 
     (вызывается обычно один раз в самом начале создания проекта)"""
@@ -27,48 +26,60 @@ def create_database():
 
 
 """РАБОТА С ПОЛЬЗОВАТЕЛЯМИ"""
-def get_user(db:Session, user_id:int):
-    """Функция, возвращающая пользователя по user_id"""
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-def get_user_by_email(db:Session, email:str):
-    """Функция, возвращающая пользователя по email"""
+async def get_user_by_email(email: str, db: Session):
+    """Асинхронная функция, для поиска пользователя среди БД по атрибуту email"""
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_users(db:Session, skip: int = 0, limit: int = 100):
-    """Функция, возвращающая всех пользователей"""
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: schemas.UserCreate):
-    """Функция, сохраняющая пользователя в БД"""
-    hashed_password=hash.bcrypt.hash(user.hashed_password)
-    db_user = models.User(email=user.email, 
-                        first_name=user.first_name,
-                        last_name=user.last_name,
-                        hashed_password=hashed_password)
-    db.add(db_user)
+async def create_user(user: schemas.UserCreate, db: Session):
+    """Асинхронная функция, для создания и сохранения в БД пользователя"""
+    user_obj = models.User(
+        email=user.email, first_name=user.first_name, last_name=user.last_name,hashed_password=hash.bcrypt.hash(user.hashed_password))
+    db.add(user_obj)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    db.refresh(user_obj)
+    return user_obj
+
+async def authenticate_user(email: str, password: str, db: Session):
+    """Асинхронная функция, аутентификации пользователя с обработкой ошибок"""
+    user = await get_user_by_email(db=db, email=email)
+    if not user:
+        return False
+    if not user.verify_password(password):
+        return False
+    return user
+
+async def create_token(user: models.User):
+    """Асинхронная функция, генерирующая JWT токен"""
+    user_obj = schemas.User.from_orm(user)
+    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+    return dict(access_token=token, token_type="bearer")
+
+async def get_current_user(db: Session = fastapi.Depends(get_db), token: str = fastapi.Depends(oauth2schema)):
+    """Асинхронная функция, возвращающая информацию о пользователе"""
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        user = db.query(models.User).get(payload["id"])
+    except:
+        raise fastapi.HTTPException(
+            status_code=401, detail="Неправильный пароль или почта")
+    return schemas.User.from_orm(user)
 
 
 """РАБОТА С ПОСТАМИ"""
-def create_post(db: Session, post: schemas.PostCreate, user_id: int):
-    """Функция, сохраняющая пост в БД"""
-    db_post = models.Post(**post.dict(), owner_id=user_id)
-    db.add(db_post)
+async def create_post(db: Session, post: schemas.PostCreate, user: schemas.User):
+    """Асинхронная функция, сохраняющая пост в БД"""
+    post = models.Post(**post.dict(), owner_id=user.id)
+    db.add(post)
     db.commit()
-    db.refresh(db_post)
-    return db_post
+    db.refresh(post)
+    return post
 
-
-def update_post(post_id: int, post: schemas.PostCreate, owner_id: int, db: Session):
-    """Функция, изменяющая существующий пост"""
+async def update_post(post_id: int, post: schemas.PostCreate, user: schemas.User, db: Session):
+    """Асинхронная функция, изменяющая существующий пост"""
     db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not db_post:
         raise fastapi.HTTPException(status_code=404, detail="Пост не найден")
-    if db_post.owner_id == owner_id:
+    if db_post.owner_id == user.id:
         db_post.title = post.title
         db_post.description = post.description
         db.commit()
@@ -76,23 +87,20 @@ def update_post(post_id: int, post: schemas.PostCreate, owner_id: int, db: Sessi
     else:
         raise fastapi.HTTPException(status_code=403, detail="Отказано в доступе")
 
-
-def get_posts(db:Session, skip: int = 0, limit: int = 100):
-    """Функция, возвращающая все посты"""
+async def get_posts(db:Session, skip: int = 0, limit: int = 100):
+    """Асинхронная функция, возвращающая все посты"""
     return db.query(models.Post).offset(skip).limit(limit).all()
 
-
-def get_post(db: Session, post_id: int):
-    """Функция, возвращающая пост по post_id"""
+async def get_post(db: Session, post_id: int):
+    """Асинхронная функция, возвращающая пост по post_id"""
     return db.query(models.Post).filter(models.Post.id == post_id).first()
 
-
-def delete_post(post_id: int, owner_id: int, db: Session):
-    """Функция, удаляющая пост вместе с комментариями"""
+async def delete_post(post_id: int, user: schemas.User, db: Session):
+    """Асинхронная функция, удаляющая пост вместе с комментариями"""
     db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not db_post:
         raise fastapi.HTTPException(status_code=404, detail="Пост не найден")
-    if db_post.owner_id == owner_id:
+    if db_post.owner_id == user.id:
         db_comments = db.query(models.Comment).filter_by(post_id=post_id).all()
         if db_comments:
             for db_comment in db_comments:
@@ -109,40 +117,35 @@ def delete_post(post_id: int, owner_id: int, db: Session):
 
 
 """РАБОТА С КОММЕНТАРИЯМИ"""
-def create_comment(db:Session, post_id: int, user_id: int, comment: schemas.CommentCreate):
-    """Функция, сохраняющая комментарий в БД"""
-    db_comment = models.Comment(text=comment.text, post_id=post_id, owner_id=user_id)
+async def create_comment(db:Session, post_id: int, user: schemas.User, comment: schemas.CommentCreate):
+    """Асинхронная функция, сохраняющая комментарий в БД"""
+    db_comment = models.Comment(text=comment.text, post_id=post_id, owner_id=user.id)
     db.add(db_comment)
     db.commit()
     db.refresh(db_comment)
     return db_comment
 
-
-def get_comments(db: Session, post_id: int):
-    """Функция, возвращающая все комментарии к конкретному посту"""
+async def get_comments(db: Session, post_id: int):
+    """Асинхронная функция, возвращающая все комментарии к конкретному посту"""
     return db.query(models.Comment).filter(models.Comment.post_id == post_id).all()
 
-
-def update_comment(comment_id: int, comment: schemas.CommentCreate, owner_id: int, db: Session):
-    """Функция, изменяющая существующий комментарий"""
+async def update_comment(comment_id: int, comment: schemas.CommentCreate, user: schemas.User, db: Session):
+    """Асинхронная функция, изменяющая существующий комментарий"""
     db_comment = db.query(models.Comment).filter(models.Comment.id == comment_id).first()
     if not db_comment:
         raise fastapi.HTTPException(status_code=404, detail="Комментарий не найден")
-    if db_comment.owner_id == owner_id:
+    if db_comment.owner_id == user.id:
         db_comment.text = comment.text
         db.commit()
         return {"message": "Комментарий успешно изменен"}
     else:
         raise fastapi.HTTPException(status_code=403, detail="Отказано в доступе")
-    
 
-def delete_comment(comment_id: int, owner_id: int, db: Session):
-    """Функция, удаляющая комментарий пользователем"""
-    db_comment = db.query(models.Comment).filter_by(owner_id = owner_id).filter(models.Comment.id == comment_id).first()
+async def delete_comment(comment_id: int, user: schemas.User, db: Session):
+    """Асинхронная функция, удаляющая комментарий пользователем"""
+    db_comment = db.query(models.Comment).filter_by(owner_id = user.id).filter(models.Comment.id == comment_id).first()
     if not db_comment:
         raise fastapi.HTTPException(status_code=404, detail="Комментарий не найден")
     db.delete(db_comment)
     db.commit()   
     return {"message", "Успешно удалено"}
-
-
--- main.py
+++ main.py
@@ -8,89 +8,83 @@ app = FastAPI()
 
 
 """РАБОТА С ПОЛЬЗОВАТЕЛЯМИ"""
-@app.post("/users/", response_model=schemas.User)
+@app.post("/api/users")
 # СОЗДАНИЕ ПОЛЬЗОВАТЕЛЯ
-def create_user(user: schemas.UserCreate, db: Session = Depends(crud.get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(crud.get_db)):
+    db_user = await crud.get_user_by_email(user.email, db)
     if db_user:
         raise HTTPException(
-            status_code=400, detail="Почта уже была использована")
-    return crud.create_user(db=db, user=user)
+            status_code=400, detail="Почта уже использована")
+    user = await crud.create_user(user, db)
 
+    return await crud.create_token(user)
 
-@app.get("/users/", response_model=list[schemas.User])
-# ВОЗВРАТ ПОЛЬЗОВАТЕЛЕЙ
-def get_users(db: Session = Depends(crud.get_db), skip: int = 0, limit: int = 100):
-    users = crud.get_users(db=db, skip=skip, limit=limit)
-    return users
+@app.post("/api/token")
+# ГЕНЕРАЦИЯ JWT ТОКЕНА
+async def generate_token(form_data: security.OAuth2PasswordRequestForm = Depends(), db: Session = Depends(crud.get_db)):
+    user = await crud.authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=401, detail="Неправильные данные")
 
+    return await crud.create_token(user)
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-# ВОЗВРАТ ПОЛЬЗОВАТЕЛЯ
-def get_user(user_id: int, db: Session = Depends(crud.get_db)):
-    db_user = crud.get_user(db=db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="Пользователь не найден")
-    return db_user
+@app.get("/api/users/me", response_model=schemas.User)
+# ВОЗВРАТ ИНФОРМАЦИИ О ПОЛЬЗОВАТЕЛЕ
+async def get_user(user: schemas.User = Depends(crud.get_current_user)):
+    return user
 
 
 """РАБОТА С ПОСТАМИ"""
-@app.post("/posts/{user_id}.", response_model=schemas.Post)
+@app.post("/posts/create_post/", response_model=schemas.Post)
 # СОЗДАНИЕ ПОСТА
-def create_post(user_id: int, post: schemas.PostCreate, db: Session = Depends(crud.get_db)):
-    return (crud.create_post(db=db, post=post, user_id=user_id))
+async def create_post(post: schemas.PostCreate, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.create_post(db=db, post=post, user=user)
+
 
 
 @app.put("/posts/update_post/{post_id}", status_code=200)
 # РЕДАКТИРОВАНИЕ ПОСТА
-def update_post(post_id: int, post: schemas.PostCreate, owner_id: int, db: Session = Depends(crud.get_db)):
-    return(crud.update_post(post_id, post, owner_id, db))
+async def update_post(post_id: int, post: schemas.PostCreate, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.update_post(post_id, post, user, db)
 
 
 @app.get("/posts/", response_model=list[schemas.Post])
 # ВОЗВРАТ ПОСТОВ
-def get_posts(db: Session = Depends(crud.get_db), skip: int = 0, limit: int = 100):
-    users = crud.get_posts(db=db, skip=skip, limit=limit)
-    return users
+async def get_posts(db: Session = Depends(crud.get_db), skip: int = 0, limit: int = 100):
+    posts = await crud.get_posts(db=db, skip=skip, limit=limit)
+    return posts
 
 
 @app.get("/posts/{post_id}", response_model=schemas.PostWithComments)
 # ВОЗВРАТ ПОСТА С КОММЕНТАРИЯМИ
-def get_post_with_comments(post_id: int, db: Session = Depends(crud.get_db)):
-    post = crud.get_post(db, post_id)
-    comments = crud.get_comments(db, post_id)
+async def get_post_with_comments(post_id: int, db: Session = Depends(crud.get_db)):
+    post = await crud.get_post(db, post_id)
+    comments = await crud.get_comments(db, post_id)
     post_with_comments = schemas.PostWithComments(post=post, comments=comments)
     return post_with_comments
 
 
 @app.delete("/posts/delete_post/{post_id}", status_code=204)
-# УДАЛЕНИЕ КОММЕНТАРИЯ
-def delete_post(post_id: int, owner_id: int, db: Session = Depends(crud.get_db)):
-    return crud.delete_post(post_id, owner_id, db)
+# УДАЛЕНИЕ ПОСТА
+async def delete_post(post_id: int, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.delete_post(post_id, user, db)
 
 
 """РАБОТА С КОММЕНТАРИЯМИ"""
 @app.post("/comments/", response_model=schemas.Comment)
 # СОЗДАНИЕ КОММЕНТАРИЯ
-def create_comment(user_id: int, post_id: int, comment: schemas.CommentCreate, db: Session = Depends(crud.get_db)):
-    return (crud.create_comment(db=db, post_id=post_id, user_id=user_id, comment=comment))
+async def create_comment(post_id: int, comment: schemas.CommentCreate, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.create_comment(db=db, post_id=post_id, user=user, comment=comment)
 
 
 @app.put("/comments/update_comment/{comment_id}", status_code=200)
 # РЕДАКТИРОВАНИЕ КОММЕНТАРИЯ
-def update_comment(comment_id: int, comment: schemas.CommentCreate, owner_id: int, db: Session = Depends(crud.get_db)):
-    return(crud.update_comment(comment_id, comment, owner_id, db))
+async def update_comment(comment_id: int, comment: schemas.CommentCreate, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.update_comment(comment_id, comment, user, db)
 
 
 @app.delete("/comments/delete_comment/{comment_id}", status_code=204)
 # УДАЛЕНИЕ КОММЕНТАРИЯ
-def delete_comment(comment_id: int, owner_id: int, db: Session = Depends(crud.get_db)):
-    return crud.delete_comment(comment_id, owner_id, db)
-
-
-
-
-
-
-
-
+async def delete_comment(comment_id: int, user: schemas.User = Depends(crud.get_current_user), db: Session = Depends(crud.get_db)):
+    return await crud.delete_comment(comment_id, user, db)
