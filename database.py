6819e21c2d8695f081e6db4ddd7f8f2fd36c8dc5
petyal <alekolen35@gmail.com>
2023-06-29 12:15:20 +0300

    code cleaned

--- database.py
+++ database.py
@@ -7,4 +7,5 @@ DATABASE_URL = "sqlite:///./database.db"
 engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative.declarative_base()
+
  
